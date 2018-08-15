01acc8586eff90f35a3ab300366a45588fa411d9
ychaban.andersen@gmail.com <rozanov.k.u@gmail.com>
2018-08-15 22:17:06 +0300

    some info

---
+++ ItalkiWEB/ItalkiWEB/Models/SuperClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ItalkiWEB.Models
+{
+    public class SuperClass
+    {
+        string s;
+        string last;
+    }
+}
